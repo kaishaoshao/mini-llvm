f5b8992b0ef23de1ad00e1bcc4180f75b12d8fbf
wtz <wangtz0607@outlook.com>
2025-03-28 14:46:37 +0800

    Chore

--- contrib/sources.sh
+++ contrib/sources.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git ls-files --cached --others --exclude-standard | grep -E '\.cpp$|\.h$' | grep -v '^third_party/'
+git ls-files --cached --others --exclude-standard | grep -E '\.c$|\.cpp$|\.h$' | grep -v '^third_party/'
