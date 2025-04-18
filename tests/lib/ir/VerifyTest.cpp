06486de1ce01aedef57954f8e52c9d92998ac8b8
wtz <wangtz0607@outlook.com>
2025-04-18 15:06:48 +0800

    [ir] Verify: Reject global variables and functions with invalid linkages

--- tests/lib/ir/VerifyTest.cpp
+++ tests/lib/ir/VerifyTest.cpp
@@ -38,6 +38,21 @@ TEST(VerifyTest, unnamedFunction) {
     EXPECT_FALSE(verify(F));
 }
 
+TEST(VerifyTest, globalVarInvalidLinkage) {
+    GlobalVar G(std::make_unique<I32>(), Linkage::kInternal);
+    G.setName("test");
+    EXPECT_FALSE(verify(G));
+}
+
+TEST(VerifyTest, functionInvalidLinkage) {
+    std::unique_ptr<FunctionType> functionType = std::make_unique<FunctionType>(
+        std::make_unique<Void>(), std::vector<std::unique_ptr<Type>>(), false
+    );
+    Function F(std::move(functionType), Linkage::kInternal);
+    F.setName("test");
+    EXPECT_FALSE(verify(F));
+}
+
 TEST(VerifyTest, selfReferentialAdd) {
     std::unique_ptr<FunctionType> functionType = std::make_unique<FunctionType>(
         std::make_unique<Void>(), std::vector<std::unique_ptr<Type>>(), false
