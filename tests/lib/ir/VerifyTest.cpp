9c9a3735a0d20cd72f14ae421d347ea2691bbe57
wtz <wangtz0607@outlook.com>
2025-04-18 14:57:32 +0800

    [ir] Verify: Reject unnamed global variables and functions

--- tests/lib/ir/VerifyTest.cpp
+++ tests/lib/ir/VerifyTest.cpp
@@ -9,10 +9,12 @@
 #include "mini-llvm/ir/Constant/I32Constant.h"
 #include "mini-llvm/ir/Constant/VoidValue.h"
 #include "mini-llvm/ir/Function.h"
+#include "mini-llvm/ir/GlobalVar.h"
 #include "mini-llvm/ir/Instruction/Add.h"
 #include "mini-llvm/ir/Instruction/Ret.h"
 #include "mini-llvm/ir/Type.h"
 #include "mini-llvm/ir/Type/FunctionType.h"
+#include "mini-llvm/ir/Type/I32.h"
 #include "mini-llvm/ir/Type/Void.h"
 #include "mini-llvm/ir/Value.h"
 #include "mini-llvm/ir/Verify.h"
@@ -20,6 +22,21 @@
 
 using namespace mini_llvm;
 using namespace mini_llvm::ir;
+
+TEST(VerifyTest, unnamedGlobalVar) {
+    GlobalVar G(std::make_unique<I32>(), Linkage::kExternal);
+    EXPECT_FALSE(verify(G));
+}
+
+TEST(VerifyTest, unnamedFunction) {
+    std::unique_ptr<FunctionType> functionType = std::make_unique<FunctionType>(
+        std::make_unique<Void>(), std::vector<std::unique_ptr<Type>>(), false
+    );
+    Function F(std::move(functionType), Linkage::kExternal);
+    BasicBlock &B = F.append();
+    B.append(std::make_shared<Ret>(std::make_shared<VoidValue>()));
+    EXPECT_FALSE(verify(F));
+}
 
 TEST(VerifyTest, selfReferentialAdd) {
     std::unique_ptr<FunctionType> functionType = std::make_unique<FunctionType>(
